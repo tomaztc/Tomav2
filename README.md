983b2ac5d7600814842375ff0c586de6d44f2769
Tomaz <tomaz.teles.carneiro@gmail.com>
2026-09-10 09:54:21 -0300

    Lab 3 corda media

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 
 [Google Colab](https://colab.research.google.com/github/tomaztc/Tomav2/blob/main/Lab3.ipynb)
 
+[Google Colab (1 aerofólio na corda média)](https://colab.research.google.com/github/tomaztc/Tomav2/blob/main/Lab3_cordamedia.ipynb)
+
 [Figuras](https://github.com/tomaztc/Tomav2/tree/main/Lab3)
--- airfoil_mod.py
+++ airfoil_mod.py
@@ -223,7 +223,7 @@ def cstfoil(Au, Al, x, N1=0.5, N2=1.0, tte=0.0, plot=False):
     # Use KS function for maximum thickness.
     # We use this instead of the max function so that it has
     # a continuous variation for gradient-based optimization.
-    rhoKS = 500
+    rhoKS = 2000
     maxt = maxt + 1/rhoKS*np.log(np.sum(np.exp(rhoKS*(tt-maxt))))
     mint = mint - 1/rhoKS*np.log(np.sum(np.exp(rhoKS*(mint-tt))))
 
